48bab39d08c3c071f359b3a5a06e07ed5a301adc
Keshav <keshav04ng@gmail.com>
2024-05-02 18:29:16 +0530

    Fix minor cors error

--- server/app.js
+++ server/app.js
@@ -11,7 +11,7 @@ app.use(cookieParser());
 
 //TODO: Change the origin to deployed frontend URL
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173", "https://mediease-client.vercel.app"],
     credentials: true
 }));
 
